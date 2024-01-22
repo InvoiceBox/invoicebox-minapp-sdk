bc77c82a87c93b3c1bfd89956a3264d00bfe00ff
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-01-22 16:38:02 +0300

    fix

--- index.ts
+++ index.ts
@@ -1,7 +1,112 @@
-export type TTest = {
-    a: number;
+type TInitMessageTo = {
+    id: string;
+    action: 'init';
 };
 
-export const a: TTest = {
-    a: 1,
+type THeightMessageTo = {
+    id: string;
+    action: 'height';
+    data: number;
 };
+
+type TDoneMessageTo = {
+    id: string;
+    action: 'done';
+};
+
+type TErrorMessageTo = {
+    id: string;
+    action: 'error';
+    data: string | null;
+};
+
+type TInitMessageFrom = {
+    id: string;
+    action: 'init';
+    data: {
+        orderContainerId: string;
+        email: string;
+        name: string;
+        phone: string;
+    };
+};
+
+type TLinkMessageTo = {
+    id: string;
+    action: 'link';
+    data: string;
+};
+
+export class Child {
+    private id: string;
+
+    private messageFromEventListener = this.messageFrom.bind(this);
+
+    private initialDataPromiseResolvers: ((initialData: TInitMessageFrom['data']) => void)[] = [];
+
+    private initailData: TInitMessageFrom['data'] | null = null;
+
+    constructor() {
+        const url = new URL(window.location.href);
+        this.id = url.searchParams.get('id') as string;
+    }
+
+    connect() {
+        window.addEventListener('message', this.messageFromEventListener);
+        this.messageTo({ id: this.id, action: 'init' });
+    }
+
+    disconnect() {
+        window.removeEventListener('message', this.messageFromEventListener);
+    }
+
+    private messageFrom(originalEvent: Event) {
+        const event = originalEvent as MessageEvent;
+        try {
+            const data = event.data as TInitMessageFrom;
+            if (data.id !== this.id) return;
+
+            if (data.action === 'init') {
+                this.initialDataPromiseResolvers.forEach((resolver) => resolver(data.data));
+                this.initialDataPromiseResolvers = [];
+                this.initailData = data.data;
+            }
+        } catch (err) {
+            // do nothing
+        }
+    }
+
+    private messageTo(
+        message: TInitMessageTo | THeightMessageTo | TDoneMessageTo | TLinkMessageTo | TErrorMessageTo,
+    ) {
+        window.parent.postMessage(message, '*');
+    }
+
+    onHeightChange(height: number) {
+        this.messageTo({ id: this.id, action: 'height', data: height });
+    }
+
+    getInitialData(): Promise<TInitMessageFrom['data']> {
+        if (this.initailData) return Promise.resolve(this.initailData);
+
+        return new Promise((resolve) => {
+            this.initialDataPromiseResolvers.push(resolve);
+        });
+    }
+
+    onDone() {
+        this.messageTo({ id: this.id, action: 'done' });
+    }
+
+    onLink(href: string) {
+        this.messageTo({ id: this.id, action: 'link', data: href });
+    }
+
+    onError(message?: string) {
+        this.messageTo({ id: this.id, action: 'error', data: message || null });
+    }
+
+    getParentOrigin() {
+        return window.parent.location.origin;
+    }
+}
